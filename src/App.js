401d6cd812466cb29f4087fbeb5248f566d1d3c0
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-01 22:49:25 -0300

    context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Welcome from "./components/Welcome";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Quiz App</h1>
+      <Welcome></Welcome>
     </div>
   );
 }
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,25 @@
 import Reacts from "react";
+
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
 import './Welcome.css'
+import Quiz from '../img/quiz.svg'
 
 const Welcome = () => {
+
+    const quizState = useContext(QuizContext);
+
+    console.log(quizState);
+
     return (
-        <div>
+        <div id="welcome">
             <h2>Seja bem-vindo!</h2>
+            <p>Clique no botão abaixo para começar:</p>
+            <button>Iniciar</button>
+            <img src={Quiz} alt="" />
         </div>
     )
 }
 
-export default Welcome
+export default Welcome;
