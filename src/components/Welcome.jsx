7712c0717fb659dbf70868d42c64e69e59e00a0d
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-11-01 21:35:54 -0300

    inicial commit

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+import Reacts from "react";
+import './Welcome.css'
+
+const Welcome = () => {
+    return (
+        <div>
+            <h2>Seja bem-vindo!</h2>
+        </div>
+    )
+}
+
+export default Welcome
